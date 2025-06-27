2c74b5ed9c40a39679c5e58fa900d543ffa6119c
Reed Piernock <agile.plan2042@snooble.com>
2025-06-26 20:25:34 -0400

    update tabs (they were autoformatted?!)

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,22 +5,22 @@
 export default async function (eleventyConfig) {}
 
 export const config = {
-  //The source uses Nunjucks, Markdown, and HTML for its pages and templates.
-  // HTML and Markdown are used for writing content, then the content is run
-  // through Nunjucks and everything is output as static HTML.
-  templateFormats: ["njk", "md", "html"],
-  htmlTemplateEngine: "njk",
-  markdownTemplateEngine: "njk",
+	//The source uses Nunjucks, Markdown, and HTML for its pages and templates.
+	// HTML and Markdown are used for writing content, then the content is run
+	// through Nunjucks and everything is output as static HTML.
+	templateFormats: ["njk", "md", "html"],
+	htmlTemplateEngine: "njk",
+	markdownTemplateEngine: "njk",
 
-  // These are the folders that 11ty will use when compiling the built site.
-  // The directories for `input` and `output` are relative to the root of the
-  // project. The directories for `data`, `includes`, and `layouts` are
-  // relative to the `input` directory, i.e. `source`.
-  dir: {
-    input: "source",
-    data: "template/data",
-    includes: "template/includes",
-    layouts: "template/layouts",
-    output: "site",
-  },
+	// These are the folders that 11ty will use when compiling the built site.
+	// The directories for `input` and `output` are relative to the root of the
+	// project. The directories for `data`, `includes`, and `layouts` are
+	// relative to the `input` directory, i.e. `source`.
+	dir: {
+		input: "source",
+		data: "template/data",
+		includes: "template/includes",
+		layouts: "template/layouts",
+		output: "site",
+	}
 };
